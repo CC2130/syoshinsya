021a5c74902fb57131525c79676cce8b153f8932
Jia Chao <jiachao2130@126.com>
2021-06-20 13:18:55 +0800

    interface-value, done

---
+++ golang/method-and-interface/interface-values.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+type I interface {
+    M()
+}
+
+type T struct {
+    s string
+}
+
+func (t *T) M() {
+    fmt.Println(t.s)
+}
+
+type MyFloat float64
+
+func (f MyFloat) M() {
+    fmt.Println(f)
+}
+
+func describe(i I) {
+    fmt.Println("Print the I interface (vaule, type)")
+    fmt.Printf("(%v, %T)\n", i, i)
+}
+
+func simple_interface_value_demo() {
+    var i I
+    i = &T{"Hello"}
+    describe(i)
+
+    i = MyFloat(math.Pi)
+    describe(i)
+}
+
+func process_nil() {
+    var i I
+    var t *T
+    i = t
+    //i.M()
+    fmt.Println(i)
+}
+
+func main() {
+    fmt.Println("Interface values:")
+
+    simple_interface_value_demo()
+
+    process_nil()
+}
