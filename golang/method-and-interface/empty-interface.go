4047f532cb05946490f3fc54cd579529322fd505
Jia Chao <jiachao2130@126.com>
2021-06-21 18:35:23 +0800

    empty interface...

---
+++ golang/method-and-interface/empty-interface.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+
+func describe(i interface{}) {
+    fmt.Printf("i, value is: %v, type is: %T\n", i, i)
+}
+
+func main() {
+    var i interface{}
+    fmt.Println("Init var i as a empty interface")
+    fmt.Println("So i can be assigned to any type....")
+    fmt.Println()
+    i = 2
+    describe(i)
+
+    i = "two"
+    describe(i)
+}
