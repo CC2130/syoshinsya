aed0bd75be6e3aab4beb109f074c017dcf8dca39
Jia Chao <jiachao2130@126.com>
2021-06-23 23:54:05 +0800

    goroutine and channel

---
+++ golang/concurrency/channel-demo.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "fmt"
+    //"time"
+)
+
+func sum(s []int, c chan int) {
+    sum := 0
+    for _, v := range s {
+        //time.Sleep(300 * time.Millisecond)
+        sum += v
+    }
+    c <- sum
+}
+
+func main() {
+    s := []int{1, 3, 7, 8, -1, 5}
+    c := make(chan int)
+    go sum(s[:len(s)/2], c)
+    go sum(s[len(s)/2:], c)
+    x, y := <-c, <-c
+    fmt.Println(x, y, x + y)
+    fmt.Println("Try to uncomment the time.Sleep above, and run sereral times")
+}
---
+++ golang/concurrency/go.mod
@@ -0,0 +1,3 @@
+module cc.cc/concurrency
+
+go 1.16
